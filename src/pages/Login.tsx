b2891ffc173916d2ac9f5d5aca063526d6d91378
AnitaMavani <anitamavani90@gmail.com>
2025-01-02 18:37:32 -0800

    changes in login file

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,8 +1,48 @@
 import React, { useState } from "react";
-
+import { Box, TextField, Container, Button } from '@mui/material';
 const Login = () => {
-    return (
+    return(
+        <>
+        <Container>
+        <Box
+         display="flex"
+         flexDirection={"column"}
+         alignItems="center"
+         justifyContent={"center"}
+         maxWidth={600}
+         margin="auto"
+         marginTop={5}
+         padding={3}
+        borderRadius={5}
+        border={"1px solid #eee"}
+        >
         <h2>Login</h2>
+        <form>
+            <TextField
+                name="email"
+                label="Email"
+                type="email"
+                margin="dense"
+                placeholder="Email"
+                variant="outlined"
+                fullWidth
+            />  
+            <TextField
+                name="password"
+                label="Password"
+                type="password"
+                margin="dense"
+                placeholder="Password"
+                variant="outlined"
+                fullWidth
+            /> 
+            <div>
+                <Button variant="contained" type="submit" sx={{margin:3}}>Submit</Button>
+            </div>
+        </form>     
+        </Box>
+        </Container>
+        </>
     );
 }
 export default Login;
